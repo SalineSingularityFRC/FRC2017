e349ab54a94122c96b46e8b40f4683be99a70feb
Alex Mah <maxhale4@gmail.com>
2017-01-27 18:36:20 -0500

    Made small logitech controller class

--- FRC2017/src/org/usfirst/frc/team5066/controller2017/SmallLogitechController.java
+++ FRC2017/src/org/usfirst/frc/team5066/controller2017/SmallLogitechController.java
@@ -2,10 +2,10 @@ package org.usfirst.frc.team5066.controller2017;
 
 import edu.wpi.first.wpilibj.Joystick;
 /** A simple Joystick subclass that makes button inputs more 
- * comprehensive and efficient when using the Logitech Controller. 
+ * comprehensive and efficient when using the Small Logitech Controller. 
  * 
  * 
- * @author Max Brenner
+ * @author Alex Mah
  *
  */
 public class SmallLogitechController extends Joystick {
@@ -14,7 +14,47 @@ public class SmallLogitechController extends Joystick {
 		super(port);
 	}
 	
-	//public boolean getTrigger() {
-		//return this.getRawButton(2);
-	//}
+	public boolean getTopLeftButton() {
+		return this.getRawButton(4);
+	}
+	
+	public boolean getTopRightButton() {
+		return this.getRawButton(5);
+	}
+	
+	public boolean getTopMiddleButton() {
+		return this.getRawButton(3);
+	}
+	
+	public boolean getTopBackButton() {
+		return this.getRawButton(2);
+	}
+	
+	public boolean getBottomFrontLeftButton() {
+		return this.getRawButton(6);
+	}
+	
+	public boolean getBottomBackLeftButton() {
+		return this.getRawButton(7);
+	}
+	
+	public boolean getBottomFrontRightButton() {
+		return this.getRawButton(11);
+	}
+	
+	public boolean getBottomBackRightButton() {
+		return this.getRawButton(10);
+	}
+	
+	public boolean getBottomPlusButton() {
+		return this.getRawButton(8);
+	}
+	
+	public boolean getBottomMinusButton() {
+		return this.getRawButton(9);
+	}
+	
+	public double getBaseSlider() {
+		return this.getRawAxis(3);
+	}
 }
