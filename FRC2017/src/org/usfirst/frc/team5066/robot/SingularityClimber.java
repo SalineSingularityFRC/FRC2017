00a18b5137d8bf35b88fe9d766c831f7f78489fa
Alex Mah <maxhale4@gmail.com>
2017-02-02 20:07:57 -0500

    Added solenoid functionality

--- FRC2017/src/org/usfirst/frc/team5066/robot/SingularityClimber.java
+++ FRC2017/src/org/usfirst/frc/team5066/robot/SingularityClimber.java
@@ -1,29 +1,36 @@
 package org.usfirst.frc.team5066.robot;
 
+import edu.wpi.first.wpilibj.Solenoid;
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SingularityClimber {
-	private CANTalon talon;
+	private Solenoid gearChanger;
+	private CANTalon climber;
 	double climberSpeedConstant;
 
-	public SingularityClimber(int port, double climberSpeed){
-		talon = new CANTalon(port);
+	public SingularityClimber(int solenoidPort, int talonPort){
+		gearChanger = new Solenoid(solenoidPort);
+		climber = new CANTalon(talonPort);
 		
 		//talon.setPosition(0.0);
 	}
 	
 	public void setSpeed(double speed){
-		//Checks for illegal value (and deports them. But now we have a wall,
-		//so this won't be a problem anymore;))
+		//Checks for illegal value (and deports them)
 		speed /= Math.max(1, Math.abs(speed));
-		talon.set(speed);
+		climber.set(speed);
+		
 		
 		//SmartDashboard.putNumber("Get climber motor position", talon.getPosition());
 	}
 	
+	public void setGear(boolean gearSwitch ){
+		gearChanger.set(true);
+	}
+	
 	public double getSpeed(){
-		return talon.get();
+		return climber.get();
 	}
 	
 	/*public double getPosition(){
