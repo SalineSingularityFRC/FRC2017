3dc0d2b31866334ba8276b10f1fc354efe634565
Stephen Jasina <stephenmathjasina@gmail.com>
2017-04-04 16:11:36 -0400

    Added to SingularityLEDs

--- FRC2017/src/org/usfirst/frc/team5066/robot/SingularityLEDs.java
+++ FRC2017/src/org/usfirst/frc/team5066/robot/SingularityLEDs.java
@@ -8,7 +8,7 @@ public class SingularityLEDs {
 	public Solenoid BotLED1, BotLED2, BotLED3, TopLED1, TopLED2;
 	
 	Timer timer;
-	boolean hasStarted;
+	boolean hasSwitched;
 	
 	public SingularityLEDs(int port1, int port2, int port3, int portT1, int portT2){
 		BotLED1 = new Solenoid(port1);
@@ -18,7 +18,7 @@ public class SingularityLEDs {
 		TopLED2 = new Solenoid(portT2);
 		
 		timer = new Timer();
-		hasStarted = false;
+		hasSwitched = false;
 	}
 	
 	public void turnOffAll(){
@@ -27,6 +27,14 @@ public class SingularityLEDs {
 		BotLED3.set(false);
 		TopLED1.set(false);
 		TopLED2.set(false);
+	}
+	
+	public void turnOnAll(){
+		BotLED1.set(true);
+		BotLED2.set(true);
+		BotLED3.set(true);
+		TopLED1.set(true);
+		TopLED2.set(true);
 	}
 	
 	public void turnOnBottom(){
@@ -55,22 +63,56 @@ public class SingularityLEDs {
 	 * To be impelemented in teleop periodic
 	 */
 	public void oscillate(){
-		if(hasStarted){
+		if(!hasSwitched){
+			timer.start();
+			hasSwitched = true;
+		}
+		
+		else if(timer.get() > 0.25){
 			TopLED1.set(false);
 			TopLED2.set(true);
 			BotLED1.set(false);
 			BotLED3.set(true);
 			
-			timer.start();
-			hasStarted = true;
 		}
-		if(timer.get() > 0.4){
+		else if(timer.get() > 0.5){
 			TopLED1.set(true);
 			TopLED2.set(false);
 			BotLED1.set(true);
 			BotLED3.set(false);
 			
-			hasStarted = false;
+			timer.reset();
+			hasSwitched = false;
+		}
+	}
+	
+	public void spin(){
+		if(!hasSwitched){
+			timer.start();
+			hasSwitched = true;
+		}
+		
+		else if(timer.get() > 0.1){
+			turnOffAll();
+			BotLED1.set(true);
+		}
+		else if(timer.get() > 0.2){
+			turnOffAll();
+			BotLED2.set(true);
+		}
+		else if(timer.get() > 0.3){
+			turnOffAll();
+			BotLED3.set(true);
+		}
+		else if(timer.get() > 0.4){
+			turnOffAll();
+			TopLED1.set(true);
+		}
+		else if(timer.get() > 0.5){
+			turnOffAll();
+			TopLED2.set(true);
+			timer.reset();
+			timer.reset();
 		}
 	}
 
