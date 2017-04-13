4b4e88236cc6552737780943050b2dad0a71f9fb
Michael Wolf <wolfhowl513@gmail.com>
2017-04-13 01:01:28 -0400

    Added Blue and Red boiler-peg-then-shoot auton modes

---
+++ FRC2017/src/org/usfirst/frc/team5066/autonomousTwo/BlueBoilerPegThenShoot.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team5066.autonomousTwo;
+
+import org.usfirst.frc.team5066.library.SingularityDrive;
+import org.usfirst.frc.team5066.robot.LowGoalShooter;
+import org.usfirst.frc.team5066.robot.SingularityIntake;
+
+public class BlueBoilerPegThenShoot extends GeneralBoilerPegThenShoot{
+	
+	public BlueBoilerPegThenShoot(){
+		super(true);
+	}
+
+	@Override
+	public void run(SingularityDrive drive, LowGoalShooter shooter, SingularityIntake intake) {
+		
+		super.run(drive, shooter, intake);
+		
+	}
+	
+}
---
+++ FRC2017/src/org/usfirst/frc/team5066/autonomousTwo/RedBoilerPegThenShoot.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team5066.autonomousTwo;
+
+import org.usfirst.frc.team5066.library.SingularityDrive;
+import org.usfirst.frc.team5066.robot.LowGoalShooter;
+import org.usfirst.frc.team5066.robot.SingularityIntake;
+
+public class RedBoilerPegThenShoot extends GeneralBoilerPegThenShoot{
+	
+	public RedBoilerPegThenShoot(){
+		super(false);
+	}
+
+	@Override
+	public void run(SingularityDrive drive, LowGoalShooter shooter, SingularityIntake intake) {
+		
+		super.run(drive, shooter, intake);
+		
+	}
+	
+	
+	
+}
