25af377aab9bac86bee77d550c6daf47ff09538b
Michael Wolf <wolfhowl513@gmail.com>
2017-04-13 00:56:21 -0400

    Added RightPeg Auton and modified generalBoilerPeg auton

--- FRC2017/src/org/usfirst/frc/team5066/autonomousTwo/GeneralBoilerPegThenShoot.java
+++ FRC2017/src/org/usfirst/frc/team5066/autonomousTwo/GeneralBoilerPegThenShoot.java
@@ -6,18 +6,27 @@ import org.usfirst.frc.team5066.robot.SingularityIntake;
 
 import edu.wpi.first.wpilibj.Timer;
 
-public abstract class GeneralBoilerPegThenShoot {
-
+public abstract class GeneralBoilerPegThenShoot extends AutonMode {
+	
+	private double angleMultiplier;
+	private boolean onBlueAlliance;
+	
+	public GeneralBoilerPegThenShoot(boolean onBlueAlliance) {
+		angleMultiplier = onBlueAlliance ? 1.0 : -1.0;
+		this.onBlueAlliance = onBlueAlliance;
+	}
+	
 	public void run(SingularityDrive drive, LowGoalShooter shooter, SingularityIntake intake) {
-
-		AutonMode leftPegMode = new LeftPeg();
-		leftPegMode.run(drive, shooter, intake);
+		
+		
+		AutonMode sidePegMode = onBlueAlliance ? new LeftPeg() : new RightPeg();
+		sidePegMode.run(drive, shooter, intake);
 
 		Timer t = new Timer();
 		t.delay(5);
 
 		drive.driveStraight(83.081, -0.4, 0.05, 10);
-		drive.rotateTo(-15.0, 8);
+		drive.rotateTo(angleMultiplier * -15.0, 8);
 		drive.driveStraight(30, -0.25, 0.05, 10);
 
 		shooter.setSpeed(true, true);
