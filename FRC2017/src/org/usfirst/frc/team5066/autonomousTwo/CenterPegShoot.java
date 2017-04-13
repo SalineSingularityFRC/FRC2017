1639897eb4233e4d290d1681f35bf7f6e326770f
Cameron Tressler <camtressler@gmail.com>
2017-04-13 00:53:42 -0400

    Created CenterPegShooot which still needs a lot of work for both sides using color of alliance

---
+++ FRC2017/src/org/usfirst/frc/team5066/autonomousTwo/CenterPegShoot.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team5066.autonomousTwo;
+
+import org.usfirst.frc.team5066.library.SingularityDrive;
+import org.usfirst.frc.team5066.robot.LowGoalShooter;
+import org.usfirst.frc.team5066.robot.SingularityIntake;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class CenterPegShoot extends AutonMode {
+
+	@Override
+	public void run(SingularityDrive drive, LowGoalShooter shooter, SingularityIntake intake) {
+		AutonMode autonMode = new CenterPeg();
+		autonMode.run(drive, shooter, intake);
+		
+		Timer.delay(4);
+		
+		drive.driveStraight(-80, 0.6, 0.06, 4);
+		drive.rotateTo(90, 5);
+		drive.driveStraight(210, 0.6, 0.06, 6);
+		//drive.rotateTo(degrees, maxTime);
+	}
+
+}
